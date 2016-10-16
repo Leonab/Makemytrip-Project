87b34b18fe207454c1463ee773d2b61bf146a185
Nabeel Khawar <leonabrocks@yahoo.com>
2016-10-16 15:45:43 +0530

    first fix

--- public/js/services/visual.js
+++ public/js/services/visual.js
@@ -1,7 +1,14 @@
 angular.module('d3visual', [])
 
-.controller('chartController',['$scope', function($scope){
+.controller('chartController',['$scope','$http', function($scope,$http){
       $scope.title = "Yolo";
+	  $scope.details = {};
+      $http.get("/api/locations")
+            .then(function(response) {
+                $scope.details = response.data;
+			    
+      });
+      console.log($scope.details);
       $scope.d3Data = [
         {name: "Greg", score:98},
         {name: "Ari", score:96},
